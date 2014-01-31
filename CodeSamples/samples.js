33e0a7653309c6b7864877302053ec652bc32c33
Josh Gough <jsgough@gmail.com>
2014-01-31 17:01:03 -0500

    Removed meta examples

--- CodeSamples/samples.js
+++ CodeSamples/samples.js
@@ -12,19 +12,5 @@
 	"content": "",
 	"mode": "csharp",
 	"url": "https://v1codesamples.azurewebsites.net/rest-1.v1/csharp/queryWithRestClient.txt"
-},
-{
-	"id": "json",
-	"title": "Meta JSON",
-	"content": "",
-	"mode": "json",
-	"url": "https:\/\/ec2-54-227-126-9.compute-1.amazonaws.com\/VersionOne\/meta.v1\/Story?accept=application\/json"
-},
-{
-	"id": "xml",
-	"title": "Meta XML",
-	"content": "",
-	"mode": "xml",
-	"url": "https:\/\/ec2-54-227-126-9.compute-1.amazonaws.com\/VersionOne\/meta.v1\/Story"
 }
 ]
