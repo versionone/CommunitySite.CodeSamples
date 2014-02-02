6f2744254cb5c8af3b270c1694573d71ab1557df
Josh Gough <jsgough@gmail.com>
2014-02-02 15:30:44 -0500

    Changed protocol to https; hope to remove errors in browser.

--- CommunitySite.CodeSamples/samples.js
+++ CommunitySite.CodeSamples/samples.js
@@ -4,13 +4,13 @@
 	"title": "Java API Query",
 	"content": "",
 	"mode": "java",
-	"url": "http://v1codesamples.azurewebsites.net/api/sample?path=rest-1.v1/java/query.java"
+	"url": "https://v1codesamples.azurewebsites.net/api/sample?path=rest-1.v1/java/query.java"
 },
 {
 	"id": "csharp",
 	"title": "C# RestClient",
 	"content": "",
 	"mode": "csharp",
-	"url": "http://v1codesamples.azurewebsites.net/api/sample?path=rest-1.v1/csharp/queryWithRestClient.cs"
+	"url": "https://v1codesamples.azurewebsites.net/api/sample?path=rest-1.v1/csharp/queryWithRestClient.cs"
 }
 ]
