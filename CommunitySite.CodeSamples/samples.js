573036ceeef9d0e944fa8f79ffd7546662ba1bda
Josh Gough <jsgough@gmail.com>
2014-02-02 15:10:52 -0500

    Updated samples to point to the SAMPLE SERVICE

--- CommunitySite.CodeSamples/samples.js
+++ CommunitySite.CodeSamples/samples.js
@@ -4,13 +4,13 @@
 	"title": "Java API Query",
 	"content": "",
 	"mode": "java",
-	"url": "https://v1codesamples.azurewebsites.net/rest-1.v1/java/query.txt"
+	"url": "http://v1codesamples.azurewebsites.net/api/sample?path=rest-1.v1/java/query.txt"
 },
 {
 	"id": "csharp",
 	"title": "C# RestClient",
 	"content": "",
 	"mode": "csharp",
-	"url": "https://v1codesamples.azurewebsites.net/rest-1.v1/csharp/queryWithRestClient.txt"
+	"url": "http://v1codesamples.azurewebsites.net/api/sample?path=rest-1.v1/csharp/queryWithRestClient.cs"
 }
 ]
