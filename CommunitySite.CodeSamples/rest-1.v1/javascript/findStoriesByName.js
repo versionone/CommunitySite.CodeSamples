475ec58feae3988d84401411ba204cc2d23dc524
Josh Gough <jsgough@gmail.com>
2014-02-03 11:23:32 -0500

    Changed rest call to http: instead of https:. There are problems with the Self-signed SSL certificate and Chrome regarding OPTIONS requests. See: http://stackoverflow.com/questions/15115746/why-chrome-cancel-cors-option-request

--- CommunitySite.CodeSamples/rest-1.v1/javascript/findStoriesByName.js
+++ CommunitySite.CodeSamples/rest-1.v1/javascript/findStoriesByName.js
@@ -1,6 +1,6 @@
 ﻿require(["https://v1codesamples.azurewebsites.net/js/ajaxQuery.js"], function (ajaxQuery) {
     var findByNameSubstring = "Newbie";
-    var server = "https://ec2-54-227-126-9.compute-1.amazonaws.com/VersionOne/rest-1.v1/Data/";
+    var server = "http://ec2-54-227-126-9.compute-1.amazonaws.com/VersionOne/rest-1.v1/Data/";
     var query = "PrimaryWorkitem?accept=application/json&sel=Name,Number&find=" + findByNameSubstring + "&findin=Name";
     // Note: console.console.log to print to the original browser console instead of the special output console.
     ajaxQuery(server, query, console.log);
