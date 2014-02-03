48e76de1324f78fbc9135b6aec21e8146c707a6f
Josh Gough <jsgough@gmail.com>
2014-02-02 21:03:39 -0500

    Abstracted out queryRun function for running ajax queries

--- CommunitySite.CodeSamples/rest-1.v1/js/findStoriesByName.js
+++ CommunitySite.CodeSamples/rest-1.v1/js/findStoriesByName.js
@@ -1,18 +1,24 @@
-﻿var findByNameSubstring = "Newbie";
+﻿function queryRun(server, query, callback, username, password) {
+    username = username || "admin";
+    password = password || "admin";
+    callback = callback || console.log;
 
-var server = "https://ec2-54-227-126-9.compute-1.amazonaws.com/VersionOne/rest-1.v1/Data/PrimaryWorkitem?accept=application/json&sel=Name,Number&find=" + findByNameSubstring + "&findin=Name";
-var username = "admin";
-var password = "admin"
-function setAuthorizationHeaders(req) { req.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password)) }
+    function setAuthorizationHeaders(req) { req.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password)) }
 
-var settings = {
-    url: server,
-    beforeSend: setAuthorizationHeaders
-    // headers: { Authorization: "Basic " + btoa(username + ":" + password) } // Should also work
-};
-// Requires jQuery to be loaded:
-$.ajax(settings).done(function (data) {
-    console.log(JSON.stringify(data, null, 4));
-}).fail(function (xhr) {
-    console.log(JSON.stringify({ErrorMessage: 'Request failed:' + xhr.responseText}));
-});
+    var settings = {
+        url: server + query,
+        beforeSend: setAuthorizationHeaders
+        // headers: { Authorization: "Basic " + btoa(username + ":" + password) } // Should also work
+    };
+    // Requires jQuery to be loaded:
+    $.ajax(settings).done(function (data) {
+        callback(JSON.stringify(data, null, 4));
+    }).fail(function (xhr) {
+        callback(JSON.stringify({ ErrorMessage: 'Request failed:' + xhr.responseText }));
+    });
+}
+
+var findByNameSubstring = "Newbie";
+var server = "https://ec2-54-227-126-9.compute-1.amazonaws.com/VersionOne/rest-1.v1/Data/";
+var query= "PrimaryWorkitem?accept=application/json&sel=Name,Number&find=" + findByNameSubstring + "&findin=Name";
+queryRun(server, query);
