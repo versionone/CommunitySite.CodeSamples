542d6e119ba2b3df3d479aa1bc6fb955d4a2ff25
Josh Gough <jsgough@gmail.com>
2014-02-02 21:20:00 -0500

    Added comment to trigger push. Come on.

--- CommunitySite.CodeSamples/rest-1.v1/js/findStoriesByName.js
+++ CommunitySite.CodeSamples/rest-1.v1/js/findStoriesByName.js
@@ -2,5 +2,5 @@
     var findByNameSubstring = "Newbie";
     var server = "https://ec2-54-227-126-9.compute-1.amazonaws.com/VersionOne/rest-1.v1/Data/";
     var query = "PrimaryWorkitem?accept=application/json&sel=Name,Number&find=" + findByNameSubstring + "&findin=Name";
-    queryRun(server, query, console.log);
+    queryRun(server, query, console.log); // uses our special console.log replacement function
 });
