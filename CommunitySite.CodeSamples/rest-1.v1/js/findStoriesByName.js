28bd0c5c97b38b64b176a099f55dc66d188f4d68
Josh Gough <jsgough@gmail.com>
2014-02-02 21:16:42 -0500

    Refactored to use requirejs for queryRun.js

--- CommunitySite.CodeSamples/rest-1.v1/js/findStoriesByName.js
+++ CommunitySite.CodeSamples/rest-1.v1/js/findStoriesByName.js
@@ -1,24 +1,6 @@
-﻿function queryRun(server, query, callback, username, password) {
-    username = username || "admin";
-    password = password || "admin";
-    callback = callback || console.log;
-
-    function setAuthorizationHeaders(req) { req.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password)) }
-
-    var settings = {
-        url: server + query,
-        beforeSend: setAuthorizationHeaders
-        // headers: { Authorization: "Basic " + btoa(username + ":" + password) } // Should also work
-    };
-    // Requires jQuery to be loaded:
-    $.ajax(settings).done(function (data) {
-        callback(JSON.stringify(data, null, 4));
-    }).fail(function (xhr) {
-        callback(JSON.stringify({ ErrorMessage: 'Request failed:' + xhr.responseText }));
-    });
-}
-
-var findByNameSubstring = "Newbie";
-var server = "https://ec2-54-227-126-9.compute-1.amazonaws.com/VersionOne/rest-1.v1/Data/";
-var query= "PrimaryWorkitem?accept=application/json&sel=Name,Number&find=" + findByNameSubstring + "&findin=Name";
-queryRun(server, query);
+﻿require(["https://v1codesamples.azurewebsites.net/js/queryRun.js"], function (queryRun) {
+    var findByNameSubstring = "Newbie";
+    var server = "https://ec2-54-227-126-9.compute-1.amazonaws.com/VersionOne/rest-1.v1/Data/";
+    var query = "PrimaryWorkitem?accept=application/json&sel=Name,Number&find=" + findByNameSubstring + "&findin=Name";
+    queryRun(server, query, console.log);
+});
