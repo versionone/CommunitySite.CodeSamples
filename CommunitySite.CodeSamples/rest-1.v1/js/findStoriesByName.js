fe4cddcc5516d107649e6f360895cfc41dc05f90
Josh Gough <jsgough@gmail.com>
2014-02-02 21:27:25 -0500

    Renamed queryRun to ajaxQuery

--- CommunitySite.CodeSamples/rest-1.v1/js/findStoriesByName.js
+++ CommunitySite.CodeSamples/rest-1.v1/js/findStoriesByName.js
@@ -1,6 +1,6 @@
-﻿require(["https://v1codesamples.azurewebsites.net/js/queryRun.js"], function (queryRun) {
+﻿require(["https://v1codesamples.azurewebsites.net/js/ajaxQuery.js"], function (ajaxQuery) {
     var findByNameSubstring = "Newbie";
     var server = "https://ec2-54-227-126-9.compute-1.amazonaws.com/VersionOne/rest-1.v1/Data/";
     var query = "PrimaryWorkitem?accept=application/json&sel=Name,Number&find=" + findByNameSubstring + "&findin=Name";
-    queryRun(server, query, console.log); // uses our special console.log replacement function
+    ajaxQuery(server, query, console.log); // uses our special console.log replacement function
 });
