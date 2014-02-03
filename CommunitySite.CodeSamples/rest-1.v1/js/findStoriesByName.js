319d5465ef1615bb4d02cc9a83d16f2dbf982e32
Josh Gough <jsgough@gmail.com>
2014-02-02 19:13:03 -0500

    Added JS sample

---
+++ CommunitySite.CodeSamples/rest-1.v1/js/findStoriesByName.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    var findByNameSubstring = "Newbie";
+    var server = "https://ec2-54-227-126-9.compute-1.amazonaws.com/VersionOne/rest-1.v1/Data/PrimaryWorkitem?accept=application/json&sel=Name,Number&find=" + findByNameSubstring + "&findin=Name";
+    var username = "admin";
+    var password = "admin"
+    function setAuthorizationHeaders(req) { req.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password)) }
+
+    var settings = {
+        url: server,
+        beforeSend: setAuthorizationHeaders
+        // headers: { Authorization: "Basic " + btoa(username + ":" + password) } // Should also work
+    };
+    // Requires jQuery to be loaded:
+    $.ajax(settings).done(function (data) {
+        beautifulJson = JSON.stringify(data, null, 4);
+        console.log(beautifulJson);
+    }).fail(function (xhr) {
+        console.log('Request failed:');
+        console.log(xhr.responseText);
+    });
+})();
