db65f3b80421b33eb4204cb47c72345b1ccb2b28
Josh Gough <jsgough@gmail.com>
2014-02-02 21:05:38 -0500

    Added requirejs bower component

---
+++ CommunitySite.CodeSamples/js/queryRun.js
@@ -0,0 +1,22 @@
+﻿define(function () {
+    function queryRun(server, query, callback, username, password) {
+        username = username || "admin";
+        password = password || "admin";
+        callback = callback || console.log;
+
+        function setAuthorizationHeaders(req) { req.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password)) }
+
+        var settings = {
+            url: server + query,
+            beforeSend: setAuthorizationHeaders
+            // headers: { Authorization: "Basic " + btoa(username + ":" + password) } // Should also work
+        };
+        // Requires jQuery to be loaded:
+        $.ajax(settings).done(function (data) {
+            callback(JSON.stringify(data, null, 4));
+        }).fail(function (xhr) {
+            callback(JSON.stringify({ ErrorMessage: 'Request failed:' + xhr.responseText }));
+        });
+    }
+    return queryRun;
+});
