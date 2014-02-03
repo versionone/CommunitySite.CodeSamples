ded120d12ae4e457377a3bcd4efe953b985ae5ea
Josh Gough <jsgough@gmail.com>
2014-02-02 21:31:51 -0500

    Deploy please.

--- CommunitySite.CodeSamples/rest-1.v1/js/findStoriesByName.js
+++ CommunitySite.CodeSamples/rest-1.v1/js/findStoriesByName.js
@@ -2,6 +2,6 @@
     var findByNameSubstring = "Newbie";
     var server = "https://ec2-54-227-126-9.compute-1.amazonaws.com/VersionOne/rest-1.v1/Data/";
     var query = "PrimaryWorkitem?accept=application/json&sel=Name,Number&find=" + findByNameSubstring + "&findin=Name";
-    // Note: console.console.log to print to the original browser console instead of the special output console!
+    // Note: console.console.log to print to the original browser console instead of the special output console.
     ajaxQuery(server, query, console.log);
 });
