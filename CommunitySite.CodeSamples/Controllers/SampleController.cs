0d07befd56388316edf81ab239b1ceacba0c23a5
Josh Gough <jsgough@gmail.com>
2014-02-02 23:11:46 -0500

    Added api/stories?topic=<topic>, like rest-1.v1 to dynamically generate samples JSON response from files on disk.

--- CommunitySite.CodeSamples/Controllers/SampleController.cs
+++ CommunitySite.CodeSamples/Controllers/SampleController.cs
@@ -7,11 +7,82 @@ using System.Net.Http;
 using System.Text;
 using System.Web.Hosting;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace CommunitySite.CodeSamples.Controllers
 {
     public class SampleController : ApiController
     {
+        [Route("api/samples")]
+        public HttpResponseMessage GetByTopic(string topic)
+        {
+            var topicPath = string.Empty;
+
+            try
+            {
+                if (string.IsNullOrWhiteSpace(topic))
+                {
+                    throw new ArgumentNullException("Please specify a valid topic parameter in the form of /sample/<topic>.");
+                }
+                topicPath = HostingEnvironment.MapPath("~/" + topic);
+                if (!Directory.Exists(topicPath))
+                {
+                    throw new FileNotFoundException("Could not find a topic named: " + topic);
+                }
+            }
+            catch (ArgumentNullException ex)
+            {
+                return CreateErrorResponse(ex);
+            }
+            catch (FileNotFoundException ex)
+            {
+                return CreateErrorResponse(ex);
+            }
+
+            var directories = new DirectoryInfo(topicPath);
+            var jArray = new JArray();
+
+            foreach (var langDir in directories.GetDirectories())
+            {
+                var examples = new JArray();
+                var files = langDir.GetFiles();
+                var exampleFiles = files.Where(f => f.Extension != ".json");
+                var rootObject = new JObject();
+                rootObject["mode"] = langDir.Name;
+                rootObject["content"] = string.Empty;
+                rootObject["id"] = langDir.Name;
+                var filePathRoot = "https://v1codesamples.azurewebsites.net/api/sample?path=" + topic + "/" + langDir.Name + "/";
+
+                foreach (var file in exampleFiles)
+                {
+                    var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(file.Name);
+                    var properties = files.First(f => f.Name == fileNameWithoutExtension + ".json");
+                    using (var reader = new StreamReader(properties.OpenRead()))
+                    {
+                        var json = reader.ReadToEnd();
+                        var serializer = new JsonSerializer();
+                        var exampleRoot = rootObject.DeepClone() as JObject;
+                        var mergeWith = JObject.Parse(json);
+                        var example = new JObject(exampleRoot.Concat(mergeWith.AsJEnumerable()));
+                        example["url"] = filePathRoot + file.Name;
+                        // TODO: multiple examples per language support
+                        //examples.Add(example);
+                        jArray.Add(example);
+                    }
+                }
+                // TODO: support multiple examples
+                //jArray.Add(examples);
+            }
+
+            var content = jArray.ToString();
+
+            var resp = new HttpResponseMessage(HttpStatusCode.OK);
+            resp.Content = new StringContent(content, Encoding.UTF8, "text/plain");
+            return resp;
+
+        }
+
         public HttpResponseMessage Get(string path)
         {
             var samplePath = string.Empty;
