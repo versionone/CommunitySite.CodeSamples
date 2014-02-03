a8edcd2b261784ea65cb7b5a54bbec6a4d7d381f
Josh Gough <jsgough@gmail.com>
2014-02-02 19:39:14 -0500

    Random fixes for JS execution

--- CommunitySite.CodeSamples/rest-1.v1/js/findStoriesByName.js
+++ CommunitySite.CodeSamples/rest-1.v1/js/findStoriesByName.js
@@ -1,4 +1,5 @@
 ﻿var findByNameSubstring = "Newbie";
+
 var server = "https://ec2-54-227-126-9.compute-1.amazonaws.com/VersionOne/rest-1.v1/Data/PrimaryWorkitem?accept=application/json&sel=Name,Number&find=" + findByNameSubstring + "&findin=Name";
 var username = "admin";
 var password = "admin"
@@ -11,8 +12,7 @@ var settings = {
 };
 // Requires jQuery to be loaded:
 $.ajax(settings).done(function (data) {
-    beautifulJson = JSON.stringify(data, null, 4);
-    console.log(beautifulJson);
+    console.log(JSON.stringify(data, null, 4));
 }).fail(function (xhr) {
     console.log('Request failed:');
     console.log(xhr.responseText);
