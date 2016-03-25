2058f9cadeb5036e96389ffcb8d605ad135883ea
Josh Gough <jsgough@gmail.com>
2016-03-25 17:07:01 -0400

    Update findStoriesByName.js

--- CommunityService/rest-1.v1/javascript/findStoriesByName.js
+++ CommunityService/rest-1.v1/javascript/findStoriesByName.js
@@ -1,7 +1,5 @@
-﻿require(["https://v1codesamples.azurewebsites.net/js/ajaxQuery.js"], function (ajaxQuery) {
-    var findByNameSubstring = "Newbie";
-    var server = "http://ec2-54-227-126-9.compute-1.amazonaws.com/VersionOne/rest-1.v1/Data/";
-    var query = "PrimaryWorkitem?accept=application/json&sel=Name,Number&find=" + findByNameSubstring + "&findin=Name";
-    // Note: console.console.log to print to the original browser console instead of the special output console.
+require(["https://v1codesamples.azurewebsites.net/js/ajaxQuery.js"], function (ajaxQuery) {
+    var server = "http://thingproxy.freeboard.io/fetch/https://www14.v1host.com/v1sdktesting/rest-1.v1/Data/";
+    var query = "Member/20?accept=application/json&sel=Name";
     ajaxQuery(server, query, console.log);
 });
