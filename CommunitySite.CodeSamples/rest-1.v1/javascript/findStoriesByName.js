b45ee37a7cd65ad883fb2b2f0be2f857241ea716
Josh Gough <jsgough@gmail.com>
2016-03-25 17:03:37 -0400

    Updated to use thingproxy.freeboard.io

--- CommunitySite.CodeSamples/rest-1.v1/javascript/findStoriesByName.js
+++ CommunitySite.CodeSamples/rest-1.v1/javascript/findStoriesByName.js
@@ -1,7 +1,6 @@
-﻿require(["https://v1codesamples.azurewebsites.net/js/ajaxQuery.js"], function (ajaxQuery) {
-    var findByNameSubstring = "CommitService";
-    var server = "http://ec2-54-227-126-9.compute-1.amazonaws.com/VersionOne/rest-1.v1/Data/";
-    var query = "PrimaryWorkitem?accept=application/json&sel=Name,Number&find=" + findByNameSubstring + "&findin=Name";
+require(["https://v1codesamples.azurewebsites.net/js/ajaxQuery.js"], function (ajaxQuery) {
+    var server = "http://thingproxy.freeboard.io/fetch/https://www14.v1host.com/v1sdktesting/rest-1.v1/Data/";
+    var query = "Member/20?accept=application/json&sel=Name";
     // Note: console.console.log to print to the original browser console instead of the special output console.
     ajaxQuery(server, query, console.log);
 });
